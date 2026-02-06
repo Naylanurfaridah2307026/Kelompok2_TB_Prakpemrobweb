07eb73cbef06cd91e8ae29c47c060b0f4df28ea9
dindaaul <dndaulp@gmail.com>
2026-02-06 12:28:33 +0700

    menambahkan routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{RecipeController, AuthController, FavoriteController};
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [RecipeController::class, 'index'])->name('recipes.index');
+Route::get('/recipes/show/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+ 
+    Route::resource('recipes', RecipeController::class)->except(['index', 'show']);
+
+    Route::post('/recipes/{recipe}/favorite', [FavoriteController::class, 'toggle'])->name('recipes.favorite');
+    Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+
+
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
